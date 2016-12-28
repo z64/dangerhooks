73debf0f2d31966b563830581ccf2522c3dd2f42
z64 <zachnowicki@gmail.com>
2016-12-28 09:12:41 -0500

    add Handler::Undocked

---
+++ src/handlers/undocked.rb
@@ -0,0 +1,11 @@
+# Triggered when a player undocks from a station
+# { "timestamp":"2016-12-22T00:21:17Z", "event":"Undocked", "StationName":"Bohme's Folly" }
+module Handler::Undocked
+  def self.handle(event)
+    Discordrb::Webhooks::Embed.new(
+      description: "Undocked from #{event['StationName']}",
+      author: {  name: "CMDR #{event['Commander']}" },
+      timestamp: Time.now
+    )
+  end
+end
