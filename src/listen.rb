b93a60f2d9ce5972180a6adf147ce3cd4b100734
z64 <zachnowicki@gmail.com>
2016-12-28 15:39:43 -0500

    derive username from ENV container, remove webhook leak lol

--- src/listen.rb
+++ src/listen.rb
@@ -9,7 +9,7 @@ require 'discordrb/webhooks'
 # Configuration
 CONFIG = OpenStruct.new YAML.load_file 'config.yaml'
 LOGGER = Logger.new $stdout
-GAME_DIR = "C:/Users/#{CONFIG.user}/Saved Games/Frontier Developments/Elite Dangerous"
+GAME_DIR = "C:/Users/#{ENV['USERNAME']}/Saved Games/Frontier Developments/Elite Dangerous"
 
 # Load webhooks
 WEBHOOKS = CONFIG.webhooks.map { |url| Discordrb::Webhooks::Client.new url: url }
