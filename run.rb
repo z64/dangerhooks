34222beef571f6427f7cfb798de3a86ee1cf55d4
Somfic <somfic@gmail.com>
2018-12-25 13:52:23 +0100

    Update run.rb

--- run.rb
+++ run.rb
@@ -3,7 +3,7 @@ require_relative 'src/journal'
 
 # Create a new journal
 # TODO: Pick up when a new journal file is made
-j = Journal.new Dir["#{GAME_DIR}/*"].last
+j = Journal.new Dir["#{GAME_DIR}/Journal.*"].last
 
 # Poll it every 0.3s
 # TODO: Make this configurable
